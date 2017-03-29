155500a3ec0c5fae57d1a087ae3e42dc1d92c0b1
BrainStone <yannick@tedworld.de>
2017-03-29 19:57:16 +0200

    Defined screen functionality

--- src/tgui/screen_base.hpp
+++ src/tgui/screen_base.hpp
@@ -3,8 +3,49 @@
 
 #include "config.hpp"
 
+#include <cstddef>
+
 namespace tgui {
 class screen_base {
+public:
+	typedef int coord_type;
+
+	struct position {
+		coord_type rows;
+		coord_type columns;
+	};
+
+	enum class color {
+		BLACK,
+		DARK_RED,
+		DARK_GREEN,
+		DARK_YELLOW,
+		DARK_BLUE,
+		DARK_MAGENTA,
+		DARK_CYAN,
+		GRAY,
+		DARK_GRAY,
+		RED,
+		GREEN,
+		YELLOW,
+		BLUE,
+		MAGENTA,
+		CYAN,
+		WHITE
+	};
+
+	virtual ~screen_base();
+
+	virtual position get_size() = 0;
+	virtual position get_cursor_position() = 0;
+	virtual bool get_cursor_visible() = 0;
+
+	virtual void set_cursor_position(const position& pos) = 0;
+	virtual void move_cursor_position(const position& pos) = 0;
+	virtual void set_color(const color& foreground, const color& background) = 0;
+	virtual void set_foreground_color(const color& foreground) = 0;
+	virtual void set_background_color(const color& background) = 0;
+	virtual void get_cursor_visible(bool visibility) = 0;
 };
 }
 
