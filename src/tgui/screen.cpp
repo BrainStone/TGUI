9e0a445d42690f75f39248a511fc240b2377b6a6
BrainStone <yannick@tedworld.de>
2017-03-31 23:06:25 +0200

    Formatting

--- src/tgui/screen.cpp
+++ src/tgui/screen.cpp
@@ -1,145 +1,137 @@
 #include "screen.hpp"
 
 namespace tgui {
-namespace screen {
+	namespace screen {
+		void set_cursor_position ( coord_type column, coord_type row ) {
+			set_cursor_position( { column, row } );
+		}
 
-void set_cursor_position(coord_type column, coord_type row) {
-	set_cursor_position( { column, row });
-}
-
-void move_cursor_position(coord_type column, coord_type row) {
-	move_cursor_position( { column, row });
-}
+		void move_cursor_position ( coord_type column, coord_type row ) {
+			move_cursor_position( { column, row } );
+		}
 
 #if defined(TGUI_LINUX)
 
-// Details
+		// Details
 
-// Actual Implementation
-position get_size() {
-	winsize w;
-	position size;
+		// Actual Implementation
+		position get_size() {
+			winsize w;
+			position size;
 
-	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+			ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 
-	size.column = w.ws_col;
-	size.row = w.ws_row;
+			size.column = w.ws_col;
+			size.row = w.ws_row;
 
-	return size;
-}
+			return size;
+		}
 
-void set_cursor_position(const position& pos) {
-	std::cout << details::csi << pos.row << ';' << pos.column << 'H';
-}
+		void set_cursor_position(const position& pos) {
+			std::cout << details::csi << pos.row << ';' << pos.column << 'H';
+		}
 
-void move_cursor_position(const position& pos) {
-	std::cout << details::csi;
+		void move_cursor_position(const position& pos) {
+			std::cout << details::csi;
 
-	if(pos.column < 0) {
-		std::cout << pos.column << 'D';
-	} else if(pos.column > 0) {
-		std::cout << pos.column << 'C';
-	}
+			if(pos.column < 0) {
+				std::cout << pos.column << 'D';
+			} else if(pos.column > 0) {
+				std::cout << pos.column << 'C';
+			}
 
-	std::cout << details::csi;
+			std::cout << details::csi;
 
-	if(pos.row < 0) {
-		std::cout << pos.row << 'A';
-	} else if(pos.row > 0) {
-		std::cout << pos.row << 'B';
-	}
-}
+			if(pos.row < 0) {
+				std::cout << pos.row << 'A';
+			} else if(pos.row > 0) {
+				std::cout << pos.row << 'B';
+			}
+		}
 
-void set_color(const color& foreground, const color& background) {
-	set_background_color(background);
-	set_foreground_color(foreground);
-}
+		void set_color(const color& foreground, const color& background) {
+			set_background_color(background);
+			set_foreground_color(foreground);
+		}
 
-void set_foreground_color(const color& foreground) {
-	int intensity = (foreground & 8)? 1 : 22;
-	int color = (foreground & 7) + 30;
+		void set_foreground_color(const color& foreground) {
+			int intensity = (foreground & 8)? 1 : 22;
+			int color = (foreground & 7) + 30;
 
-	std::cout << details::csi << intensity << ';' << color << 'm';
-}
+			std::cout << details::csi << intensity << ';' << color << 'm';
+		}
 
-void set_background_color(const color& background) {
-	int intensity = (background & 8)? 1 : 22;
-	int color = (background & 7) + 40;
+		void set_background_color(const color& background) {
+			int intensity = (background & 8)? 1 : 22;
+			int color = (background & 7) + 40;
 
-	std::cout << details::csi << intensity << ';' << color << 'm';
-}
+			std::cout << details::csi << intensity << ';' << color << 'm';
+		}
 
 #elif defined(TGUI_WINDOWS)
 
-// Details
-CONSOLE_SCREEN_BUFFER_INFO details::get_console_screen_buffer_info() {
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
+		// Details
+		CONSOLE_SCREEN_BUFFER_INFO details::get_console_screen_buffer_info () {
+			CONSOLE_SCREEN_BUFFER_INFO csbi;
 
-	GetConsoleScreenBufferInfo(hStdout, &csbi);
+			GetConsoleScreenBufferInfo( hStdout, &csbi );
 
-	return csbi;
-}
+			return csbi;
+		}
 
-position details::get_cursor_position() {
-	CONSOLE_SCREEN_BUFFER_INFO csbi = details::get_console_screen_buffer_info();
-	position size;
+		position details::get_cursor_position () {
+			CONSOLE_SCREEN_BUFFER_INFO csbi = details::get_console_screen_buffer_info();
+			position size;
 
-	size.column = csbi.dwCursorPosition.X;
-	size.row = csbi.dwCursorPosition.Y;
+			size.column = csbi.dwCursorPosition.X;
+			size.row = csbi.dwCursorPosition.Y;
 
-	return size;
-}
+			return size;
+		}
 
-// Actual Implementation
-position get_size() {
-	CONSOLE_SCREEN_BUFFER_INFO csbi = details::get_console_screen_buffer_info();
-	position size;
+		// Actual Implementation
+		position get_size () {
+			CONSOLE_SCREEN_BUFFER_INFO csbi = details::get_console_screen_buffer_info();
+			position size;
 
-	size.column = csbi.dwMaximumWindowSize.X;
-	size.row = csbi.dwMaximumWindowSize.Y;
+			size.column = csbi.dwMaximumWindowSize.X;
+			size.row = csbi.dwMaximumWindowSize.Y;
 
-	return size;
-}
+			return size;
+		}
 
-void set_cursor_position(const position& pos) {
-	COORD cursor( { pos.row, pos.column });
+		void set_cursor_position ( const position& pos ) {
+			COORD cursor( { pos.row, pos.column } );
 
-	SetConsoleCursorPosition(details::hStdout, cursor);
-}
+			SetConsoleCursorPosition( details::hStdout, cursor );
+		}
 
-void move_cursor_position(const position& pos) {
-	position cur_pos = details::get_cursor_position();
+		void move_cursor_position ( const position& pos ) {
+			position cur_pos = details::get_cursor_position();
 
-	set_cursor_position(cur_pos.row + pos.row, cur_pos.column + pos.column);
-}
+			set_cursor_position( cur_pos.row + pos.row, cur_pos.column + pos.column );
+		}
 
-void set_color(const color& foreground, const color& background) {
-	SetConsoleTextAttribute(details::hStdout,
-			details::foreground_colors.at(foreground)
-					| details::background_colors.at(background));
-}
+		void set_color ( const color& foreground, const color& background ) {
+			SetConsoleTextAttribute( details::hStdout, details::foreground_colors.at( foreground ) | details::background_colors.at( background ) );
+		}
 
-void set_foreground_color(const color& foreground) {
-	SetConsoleTextAttribute(details::hStdout,
-			(details::get_console_screen_buffer_info().wAttributes & 0xf0)
-					| details::foreground_colors.at(foreground));
-}
+		void set_foreground_color ( const color& foreground ) {
+			SetConsoleTextAttribute( details::hStdout, (details::get_console_screen_buffer_info().wAttributes & 0xf0) | details::foreground_colors.at( foreground ) );
+		}
 
-void set_background_color(const color& background) {
-	SetConsoleTextAttribute(details::hStdout,
-			(details::get_console_screen_buffer_info().wAttributes & 0x0f)
-					| details::background_colors.at(background));
-}
+		void set_background_color ( const color& background ) {
+			SetConsoleTextAttribute( details::hStdout, (details::get_console_screen_buffer_info().wAttributes & 0x0f) | details::background_colors.at( background ) );
+		}
 
 #endif
 
-void ring_bell() {
-	std::cout << '\a';
-}
+		void ring_bell () {
+			std::cout << '\a';
+		}
 
-void flush_screen() {
-	std::cout.flush();
-}
-
-}
+		void flush_screen () {
+			std::cout.flush();
+		}
+	}
 }
--- src/tgui/screen.hpp
+++ src/tgui/screen.hpp
@@ -20,104 +20,72 @@
 #endif
 
 namespace tgui {
-namespace screen {
-typedef short coord_type;
+	namespace screen {
+		typedef short coord_type;
 
-struct position {
-	coord_type column;
-	coord_type row;
-};
+		struct position {
+			coord_type column;
+			coord_type row;
+		};
 
-enum class color {
-	BLACK,
-	DARK_RED,
-	DARK_GREEN,
-	DARK_YELLOW,
-	DARK_BLUE,
-	DARK_MAGENTA,
-	DARK_CYAN,
-	GRAY,
-	DARK_GRAY,
-	RED,
-	GREEN,
-	YELLOW,
-	BLUE,
-	MAGENTA,
-	CYAN,
-	WHITE
-};
+		enum class color {
+			BLACK, DARK_RED, DARK_GREEN, DARK_YELLOW, DARK_BLUE, DARK_MAGENTA, DARK_CYAN, GRAY, DARK_GRAY, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE
+		};
 
-position get_size();
-bool get_cursor_visible();
+		position get_size ();
+		bool get_cursor_visible ();
 
-void set_cursor_position(const position& pos);
-void set_cursor_position(coord_type column, coord_type row);
-void move_cursor_position(const position& pos);
-void move_cursor_position(coord_type column, coord_type row);
-void set_color(const color& foreground, const color& background);
-void set_foreground_color(const color& foreground);
-void set_background_color(const color& background);
-void set_cursor_visible(bool visible);
+		void set_cursor_position ( const position& pos );
+		void set_cursor_position ( coord_type column, coord_type row );
+		void move_cursor_position ( const position& pos );
+		void move_cursor_position ( coord_type column, coord_type row );
+		void set_color ( const color& foreground, const color& background );
+		void set_foreground_color ( const color& foreground );
+		void set_background_color ( const color& background );
+		void set_cursor_visible ( bool visible );
 
-void clear_screen();
-void flash_screen();
-void ring_bell();
-void flush_screen();
+		void clear_screen ();
+		void flash_screen ();
+		void ring_bell ();
+		void flush_screen ();
+
+		namespace details {
 
 #if defined(TGUI_LINUX)
 
-namespace details {
-const std::string csi = "\033[";
-}
+			const std::string csi = "\033[";
 
 #elif defined(TGUI_WINDOWS)
 
-namespace details {
-const HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
-const std::map<color, WORD> foreground_colors(
-		{ { color::BLACK, 0 }, { color::DARK_RED, FOREGROUND_RED }, {
-				color::DARK_GREEN, FOREGROUND_GREEN }, { color::DARK_YELLOW,
-		FOREGROUND_RED | FOREGROUND_GREEN }, { color::DARK_BLUE,
-		FOREGROUND_BLUE }, { color::DARK_MAGENTA, FOREGROUND_RED
-				| FOREGROUND_BLUE }, { color::DARK_CYAN, FOREGROUND_GREEN
-				| FOREGROUND_BLUE }, { color::GRAY, FOREGROUND_RED
-				| FOREGROUND_GREEN | FOREGROUND_BLUE }, { color::DARK_GRAY,
-		FOREGROUND_INTENSITY }, { color::RED, FOREGROUND_RED
-				| FOREGROUND_INTENSITY }, { color::GREEN, FOREGROUND_GREEN
-				| FOREGROUND_INTENSITY }, { color::YELLOW,
-		FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY }, {
-				color::BLUE,
-				FOREGROUND_BLUE | FOREGROUND_INTENSITY }, { color::MAGENTA,
-		FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_INTENSITY }, {
-				color::CYAN, FOREGROUND_GREEN | FOREGROUND_BLUE
-						| FOREGROUND_INTENSITY }, { color::WHITE, FOREGROUND_RED
-				| FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY } });
-const std::map<color, WORD> background_colors(
-		{ { color::BLACK, 0 }, { color::DARK_RED, BACKGROUND_RED }, {
-				color::DARK_GREEN, BACKGROUND_GREEN }, { color::DARK_YELLOW,
-		BACKGROUND_RED | BACKGROUND_GREEN }, { color::DARK_BLUE,
-		BACKGROUND_BLUE }, { color::DARK_MAGENTA, BACKGROUND_RED
-				| BACKGROUND_BLUE }, { color::DARK_CYAN, BACKGROUND_GREEN
-				| BACKGROUND_BLUE }, { color::GRAY, BACKGROUND_RED
-				| BACKGROUND_GREEN | BACKGROUND_BLUE }, { color::DARK_GRAY,
-		BACKGROUND_INTENSITY }, { color::RED, BACKGROUND_RED
-				| BACKGROUND_INTENSITY }, { color::GREEN, BACKGROUND_GREEN
-				| BACKGROUND_INTENSITY }, { color::YELLOW,
-		BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_INTENSITY }, {
-				color::BLUE,
-				BACKGROUND_BLUE | BACKGROUND_INTENSITY }, { color::MAGENTA,
-		BACKGROUND_RED | BACKGROUND_BLUE | BACKGROUND_INTENSITY }, {
-				color::CYAN, BACKGROUND_GREEN | BACKGROUND_BLUE
-						| BACKGROUND_INTENSITY }, { color::WHITE, BACKGROUND_RED
-				| BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY } });
+			const HANDLE hStdout = GetStdHandle( STD_OUTPUT_HANDLE );
+			const std::map<color, WORD> foreground_colors(
+					{ { color::BLACK, 0 }, { color::DARK_RED, FOREGROUND_RED }, { color::DARK_GREEN, FOREGROUND_GREEN }, { color::DARK_YELLOW,
+					FOREGROUND_RED | FOREGROUND_GREEN }, { color::DARK_BLUE,
+					FOREGROUND_BLUE }, { color::DARK_MAGENTA, FOREGROUND_RED | FOREGROUND_BLUE }, { color::DARK_CYAN, FOREGROUND_GREEN | FOREGROUND_BLUE }, { color::GRAY, FOREGROUND_RED
+							| FOREGROUND_GREEN | FOREGROUND_BLUE }, { color::DARK_GRAY,
+					FOREGROUND_INTENSITY }, { color::RED, FOREGROUND_RED | FOREGROUND_INTENSITY }, { color::GREEN, FOREGROUND_GREEN | FOREGROUND_INTENSITY }, { color::YELLOW,
+					FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY }, { color::BLUE,
+					FOREGROUND_BLUE | FOREGROUND_INTENSITY }, { color::MAGENTA,
+					FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_INTENSITY }, { color::CYAN, FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY }, { color::WHITE, FOREGROUND_RED
+							| FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY } } );
+			const std::map<color, WORD> background_colors(
+					{ { color::BLACK, 0 }, { color::DARK_RED, BACKGROUND_RED }, { color::DARK_GREEN, BACKGROUND_GREEN }, { color::DARK_YELLOW,
+					BACKGROUND_RED | BACKGROUND_GREEN }, { color::DARK_BLUE,
+					BACKGROUND_BLUE }, { color::DARK_MAGENTA, BACKGROUND_RED | BACKGROUND_BLUE }, { color::DARK_CYAN, BACKGROUND_GREEN | BACKGROUND_BLUE }, { color::GRAY, BACKGROUND_RED
+							| BACKGROUND_GREEN | BACKGROUND_BLUE }, { color::DARK_GRAY,
+					BACKGROUND_INTENSITY }, { color::RED, BACKGROUND_RED | BACKGROUND_INTENSITY }, { color::GREEN, BACKGROUND_GREEN | BACKGROUND_INTENSITY }, { color::YELLOW,
+					BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_INTENSITY }, { color::BLUE,
+					BACKGROUND_BLUE | BACKGROUND_INTENSITY }, { color::MAGENTA,
+					BACKGROUND_RED | BACKGROUND_BLUE | BACKGROUND_INTENSITY }, { color::CYAN, BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY }, { color::WHITE, BACKGROUND_RED
+							| BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY } } );
 
-CONSOLE_SCREEN_BUFFER_INFO get_console_screen_buffer_info();
-position get_cursor_position();
-}
+			CONSOLE_SCREEN_BUFFER_INFO get_console_screen_buffer_info ();
+			position get_cursor_position ();
 
 #endif
 
-}
+		}
+	}
 }
 
 #endif // #ifndef TGUI_TGUI_SCREEN_HPP
