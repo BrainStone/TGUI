5f3d33070a086b0bbac87857622e5fba68cc112c
BrainStone <yannick@tedworld.de>
2017-04-01 00:09:12 +0200

    Added application.hpp

--- src/tgui.hpp
+++ src/tgui.hpp
@@ -2,6 +2,7 @@
 #define TGUI_TGUI_HPP
 
 #include "tgui/config.hpp"
+#include "tgui/application.hpp"
 #include "tgui/screen.hpp"
 
 #endif // #ifndef TGUI_TGUI_HPP
---
+++ src/tgui/application.hpp
@@ -0,0 +1,7 @@
+#ifndef TGUI_TGUI_APPLICATION_HPP
+#define TGUI_TGUI_APPLICATION_HPP
+
+namespace tgui {
+}
+
+#endif // #ifndef TGUI_TGUI_APPLICATION_HPP
