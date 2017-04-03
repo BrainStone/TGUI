74b8891b3ff73ee236257e801e1aed7b6da8bbc6
BrainStone <yannick@tedworld.de>
2017-04-03 17:58:30 +0200

    Added some documentation

--- src/tgui.hpp
+++ src/tgui.hpp
@@ -1,6 +1,12 @@
 #ifndef TGUI_TGUI_HPP
 #define TGUI_TGUI_HPP
 
+/**
+ * @file tgui.hpp
+ *
+ * This file is the main header for the TGUI Library. It is recommended to only include this file.
+ */
+
 #include "tgui/config.hpp"
 #include "tgui/application.hpp"
 #include "tgui/screen_buffer.hpp"
--- src/tgui/config.hpp
+++ src/tgui/config.hpp
@@ -9,4 +9,21 @@
 #error Unsupported System!
 #endif
 
+/**
+ * The namepsace for the TGUI Library. All methods and classes will be located inside this namespace or sub-namespaces.
+ */
+namespace tgui {
+	/**
+	 * The namespace for the screen and console utilities. This is the only place where platform dependend code is located.
+	 */
+	namespace screen {
+		/**
+		 * This namespace contains platform specific implementations.<br />
+		 * <b>It is not recommended to use any methods inside this namespace!</b>
+		 */
+		namespace details {
+		}
+	}
+}
+
 #endif // #ifndef TGUI_TGUI_CONFIG_HPP
