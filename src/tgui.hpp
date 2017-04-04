78463820c686e30785a34f6c842c80660ba5f531
BrainStone <yannick@tedworld.de>
2017-04-04 16:36:44 +0200

    Updated Doxyfile

--- src/tgui.hpp
+++ src/tgui.hpp
@@ -2,6 +2,13 @@
 #define TGUI_TGUI_HPP
 
 /**
+ * @mainpage
+ *
+ * @copyright Copyright 2017 Yannick Schinko. All rights reserved
+ * @license TGUI is licensed under the GNU General Public License v3.0
+ */
+
+/**
  * @file tgui.hpp
  *
  * This file is the main header for the TGUI Library. It is recommended to only include this file.
